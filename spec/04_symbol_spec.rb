c79e92c6ab82197bbdeed44bd3da2850ae502f91
Theresa Solak <yarfreese@gmail.com>
2013-01-15 19:48:18 -0800

    Updated Gemfile and specs

--- spec/04_symbol_spec.rb
+++ spec/04_symbol_spec.rb
@@ -8,14 +8,36 @@ describe "Symbols" do
     ("only" + "one").to_sym.object_id.should eq :onlyone.object_id
   end
 
-  it "are of class Symbol"
+  it "are of class Symbol" do
+    :my_symbol.should be_a Symbol
+  end
   
-  it "are not the same as a string of the same characters"
+  it "are not the same as a string of the same characters" do
+    str = "my gossamer string"
+    sym = str.to_sym
+    # sym.should eq str
+    sym.should_not eq str
+  end
   
-  it "are convertable to strings"
+  it "are convertable to strings" do
+    str = "my flexible string"
+    sym = str.to_sym
+    new_str = sym.to_s
+    new_str.should eq str
+  end
 
-  it "are convertable from a string"
+  it "are convertable from a string" do
+    str = "my convertable string"
+    sym = str.to_sym
+    sym.should be_a Symbol
+  end
 
-  it "automatically converts to a string when interpolated"
+  it "automatically converts to a string when interpolated" do
+    str = "this is fun"
+    sym = str.to_sym
+    "#{sym}".should eq "this is fun"
+    "#{:"this is fun"}".should eq "this is fun"
+    # "#{:"this is fun"}".should_not eq "this is fun"
+  end
   
 end
